83fe28973121b7421523dc350a97f09c6f02bb0d
Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>
2020-07-19 20:43:18 +0200

    HACK: probe all clk devices on boot

--- board/samsung/zero/zero.c
+++ board/samsung/zero/zero.c
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0+
 
 #include <common.h>
+#include <dm/uclass.h>
+#include <dm/device.h>
 
 int exynos_init(void)
 {
+	struct udevice *dev;
+
+	puts("Probing all clock devices\n");
+	for (uclass_first_device(UCLASS_CLK, &dev);
+	     dev;
+	     uclass_next_device(&dev)) {
+		pr_debug("probe pincontrol = %s\n", dev->name);
+	}
+
 	return 0;
 }
